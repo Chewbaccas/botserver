684c5c19eae792aca0c48a6ee2ae633a20ef5f66
Guilherme Dio <ggrdio@gmail.com>
2016-12-11 06:53:55 -0200

    Modified cors, still not working

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,31 @@
 	});
 	server.use(restify.queryParser());
 	server.use(restify.bodyParser());
+
+	function corsHandler(req, res, next) {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token');
+		res.setHeader('Access-Control-Allow-Methods', '*');
+		res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+		res.setHeader('Access-Control-Max-Age', '1000');
+		return next();
+	}
+
+	function optionsRoute(req, res, next) {
+		res.send(200);
+		return next();
+	}
+
+
+	server.use(restify.CORS({
+		origins: ['http://127.0.0.1', 'http://localhost'],
+		credentials: true,
+		headers: ['X-Api-Version', 'X-Request-Id', 'X-Response-Time'],
+		methods: ['GET','PUT','DELETE','POST','OPTIONS']
+	}));
+
+
+	server.opts('/\.*/', corsHandler, optionsRoute);
 	 
 	// main
 	server.post('/chat/', function (req, res, next) {
@@ -29,14 +54,7 @@
 	});
 
 	function handleRequest(req, res, next) {
-		res.setHeader('Access-Control-Allow-Origin', '*');
-		res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
-		res.setHeader('Access-Control-Allow-Methods', '*');
-		res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
-		res.setHeader('Access-Control-Max-Age', '1000');
-		console.log(res);
 		var answerText = bot.handle(req.params.question);
-		console.log(answerText);
 	  	res.send(new Answer(answerText));
 	  	return next();
 	}
